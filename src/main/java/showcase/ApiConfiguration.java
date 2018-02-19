5f0472dcbfc44b12f7ff54fdec7b64b5bfd674c5
tra <tra@kb.dk>
2018-02-19 17:30:44 +0100

    added "{id}" endpoint, and index.html linking to endpoints.

--- src/main/java/showcase/ApiConfiguration.java
+++ src/main/java/showcase/ApiConfiguration.java
@@ -6,6 +6,6 @@ import javax.ws.rs.core.Application;
 /**
  * @author Pavel Pscheidl <pavel.junior@pscheidl.cz>
  */
-@ApplicationPath("")
+@ApplicationPath("rest")
 public class ApiConfiguration extends Application {
 }
--- src/main/java/showcase/SampleEndpoint.java
+++ src/main/java/showcase/SampleEndpoint.java
@@ -2,6 +2,7 @@ package showcase;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
 /**
@@ -13,5 +14,11 @@ public class SampleEndpoint {
     @GET
     public Response getSample() {
         return Response.ok("Sample response").build();
+    }
+
+    @GET
+    @Path("{id}")
+    public Response getSample(@PathParam("id") String id) {
+        return Response.ok("Sample response: " + id).build();
     }
 }
